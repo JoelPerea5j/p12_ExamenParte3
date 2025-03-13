386b0f8958b9fc7257de349662bf7925d7a354a2
JOEL_ALBERTO_PEREA_CASTORENA <a.22308051281272@cbtis128.edu.mx>
2025-03-13 18:51:04 +0000

    Perea Joel

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,98 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+class Cliente {
+  final String nombre;
+  final String apellido;
+  final String ine;
+  final String domicilio;
+  final String fechaNacimiento;
+  Cliente({
+    required this.nombre,
+    required this.apellido,
+    required this.ine,
+    required this.domicilio,
+    required this.fechaNacimiento,
+  });
+}
+
+void main() {
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Perea Columna stack'),
+        ),
+        body: ClienteList(),
       ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
+class ClienteList extends StatelessWidget {
+  final List<Cliente> clientes = [
+    Cliente(
+      nombre: 'Juan',
+      apellido: 'Pérez',
+      ine: '1234567890',
+      domicilio: 'Calle Principal 123',
+      fechaNacimiento: '01/01/1990',
+    ),
+    Cliente(
+      nombre: 'María',
+      apellido: 'Gómez',
+      ine: '0987654321',
+      domicilio: 'Avenida Secundaria 456',
+      fechaNacimiento: '02/02/1995',
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
-      ),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: clientes.map((cliente) {
+        return Column(
+          children: [
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 10),
+              padding: EdgeInsets.all(16),
+              decoration: BoxDecoration(
+                color: clientes.indexOf(cliente) % 2 == 0
+                    ? Colors.blue[100]
+                    : Colors.green[100],
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 2,
+                    blurRadius: 5,
+                    offset: Offset(0, 3),
+                  ),
+                ],
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('Nombre: ${cliente.nombre} ${cliente.apellido}'),
+                  Text('INE: ${cliente.ine}'),
+                  Text('Domicilio: ${cliente.domicilio}'),
+                  Text('Fecha de Nacimiento: ${cliente.fechaNacimiento}'),
+                ],
+              ),
+            ),
+            if (clientes.indexOf(cliente) < clientes.length - 1)
+              Divider(
+                color: Colors.grey,
+                thickness: 1,
+              ),
+          ],
+        );
+      }).toList(),
     );
   }
 }
